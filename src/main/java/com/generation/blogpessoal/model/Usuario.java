8d168c9d18b0b83507400b2ba6c70e3e779bc087
Camila Andrade <camila.rda@outlook.com>
2024-07-18 16:05:37 -0300

    implementando testes JUnit

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -91,4 +91,15 @@ public class Usuario {
 		this.postagem = postagem;
 	}
 
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+
+	public Usuario() {
+	}
+
 }
